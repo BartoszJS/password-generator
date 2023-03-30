0942397ee73bbbbfb8dc729e9075a6bec9b7b32d
BartoszJS <bartosz290300@wp.pl>
2023-03-30 15:42:42 +0200

    finish-generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import PasswordInput from "./components/PasswordInput";
+import "./index.css";
 
-function App() {
+const App: React.FC = () => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const handleSuccess = () => {
+    setShowModal(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {showModal && (
+        <div className='absolute w-full h-full z-10 bg-black/40 flex justify-center items-center'>
+          <div className='w-[300px] h-[100px] text-xl bg-white rounded flex flex-col justify-center items-center'>
+            Prawidłowe hasło
+            <button
+              className='bg-blue-500 rounded mt-1 text-l py-1 cursor-pointer px-2'
+              onClick={() => setShowModal(!showModal)}
+            >
+              OK
+            </button>
+          </div>
+        </div>
+      )}
+      <PasswordInput password='dlugosc-h' onSuccess={handleSuccess} />
     </div>
   );
-}
+};
 
 export default App;
